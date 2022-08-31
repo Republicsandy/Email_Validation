155e59dc1cb815907dc1b9ee281cdc05100cc63e
Sandeep singh <sandy180388@gmail.com>
2022-08-31 16:42:38 +0530

    [sandeep] valied email file added in UC_2

---
+++ Email_Validation/Email_Validation1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Email_Validation
+{
+    class Email_Validation1
+    {
+        //pattern for email check
+        public static string REGEX_EMAIL = "^[a-zA-Z0-9]+[@][a-zA-Z0-9]+";
+        public bool validateEmail1(string email1)
+        {
+            return Regex.IsMatch(email1, REGEX_EMAIL);
+        }
+    }
+}
--- Email_Validation/Program.cs
+++ Email_Validation/Program.cs
@@ -9,7 +9,8 @@ namespace Email_Validation
         {
             Console.WriteLine("Welcome to Email Validation!");
             Console.WriteLine("press 1 for email validation !");
-           
+            Console.WriteLine("press 2 for second check email validation !");
+
             var getValueByUser = Convert.ToInt32(Console.ReadLine());
             switch (getValueByUser)
             {
@@ -19,6 +20,13 @@ namespace Email_Validation
                     string email = Console.ReadLine();
                     bool valid = validation.validateEmail(email);
                     Console.WriteLine(valid);
+                    break;
+                case 2:
+                    Email_Validation1 validation1 = new Email_Validation1();
+                    Console.WriteLine("Enter you email");
+                    string email1 = Console.ReadLine();
+                    bool valid1 = validation1.validateEmail1(email1);
+                    Console.WriteLine(valid1);
                     break;
 
                 default:
