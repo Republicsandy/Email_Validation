cced1d9895f941a44c8fdc77611e0496f75eec04
Sandeep singh <sandy180388@gmail.com>
2022-08-31 16:50:20 +0530

    [sandeep] valied email file added in UC_4

---
+++ Email_Validation/Email_Validation3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Email_Validation
+{
+    class Email_Validation3
+    {
+        //pattern check
+        public static string REGEX_EMAIL = "^[a-zA-Z0-9]+([.+-_#$][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}$";
+        public bool validateEmail3(string email3)
+        {
+            return Regex.IsMatch(email3, REGEX_EMAIL);
+        }
+    }
+}
--- Email_Validation/Program.cs
+++ Email_Validation/Program.cs
@@ -11,6 +11,7 @@ namespace Email_Validation
             Console.WriteLine("press 1 for email validation !");
             Console.WriteLine("press 2 for second check email validation !");
             Console.WriteLine("press 3 for third check email validation !");
+            Console.WriteLine("press 4 for fourth check email validation !");
 
             var getValueByUser = Convert.ToInt32(Console.ReadLine());
             switch (getValueByUser)
@@ -35,6 +36,13 @@ namespace Email_Validation
                     string email2 = Console.ReadLine();
                     bool valid2 = validation2.validateEmail2(email2);
                     Console.WriteLine(valid2);
+                    break;
+                case 4:
+                    Email_Validation3 validation3 = new Email_Validation3();
+                    Console.WriteLine("Enter you email");
+                    string email3 = Console.ReadLine();
+                    bool valid3 = validation3.validateEmail3(email3);
+                    Console.WriteLine(valid3);
                     break;
 
                 default:
