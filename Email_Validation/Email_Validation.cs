e278a344f27d5561f5cc5a95b909033633fad9fb
Sandeep singh <sandy180388@gmail.com>
2022-08-31 16:36:38 +0530

    [sandeep] valied email file added in UC_1

---
+++ Email_Validation/Email_Validation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Email_Validation
+{
+    class Email_Validation
+    {
+        //pattern for email
+        public static string REGEX_EMAIL = "^[a-zA-Z0-9]+";
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+    }
+}
---
+++ Email_Validation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Email_Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Email Validation!");
+            Console.WriteLine("press 1 for email validation !");
+           
+            var getValueByUser = Convert.ToInt32(Console.ReadLine());
+            switch (getValueByUser)
+            {
+                case 1:
+                    Email_Validation validation = new Email_Validation();
+                    Console.WriteLine("Enter you email");
+                    string email = Console.ReadLine();
+                    bool valid = validation.validateEmail(email);
+                    Console.WriteLine(valid);
+                    break;
+
+                default:
+                    Console.WriteLine("invalid number");
+                    return;
+            }
+        }
+    }
+}
